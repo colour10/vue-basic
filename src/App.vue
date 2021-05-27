961d0de4eaaf355ebc7a6c6b6c13458eed9a1459
刘宗阳 <84299915@qq.com>
2021-05-28 01:08:54 +0800

    初始化

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,26 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<template lang="pug">
+  div#app
+    img(src="./assets/logo.png")
+    h1 Welcome from pug & sass
 </template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
+  margin-top: 200px;
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  h1 {
+    color: #42b983;
   }
 }
 </style>
